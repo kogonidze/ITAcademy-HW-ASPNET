401c29b1adb300d1193a9e20454c87c31c59f366
kogonidze <kottok1998@gmail.com>
2021-07-13 23:45:30 +0300

    + created SimpleAuthorization app

---
+++ hw2/SimpleAuthorization/SimpleAuthorization/Authorization.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Authentication;
+using System.Text;
+
+namespace SimpleAuthorization
+{
+    public class Authorization
+    {
+        private static Dictionary<string, string> authDictionary;
+
+        public static UserModel Run(string[] args)
+        {
+            FillAuthDictionary();
+
+            var login = args[0];
+
+            var passwd = args[1];
+
+            if (!authDictionary.ContainsKey(login))
+            {
+                throw new AuthenticationException();
+            }
+
+            var realPasswd = authDictionary[login];
+
+            if (passwd != realPasswd)
+            {
+                throw new AuthenticationException();
+            }
+
+            return new UserModel() {Username = login};
+        }
+
+        private static void FillAuthDictionary()
+        {
+            authDictionary.Add("admin", "root");
+            authDictionary.Add("user1", "kiTTy2001");
+        }
+    }
+}
---
+++ hw2/SimpleAuthorization/SimpleAuthorization/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleAuthorization
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+    }
+}
