99053badb2395556141b40e34d8ecfc7b0471ce4
kogonidze <kottok1998@gmail.com>
2021-07-19 13:12:24 +0300

    + added console app with violation of LSP principle

---
+++ hw3/WithoutLSP/WithoutLSP/RubberDuck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WithoutLSP
+{
+    public class RubberDuck: Duck
+    {
+        public override void Quack()
+        {
+            Console.WriteLine("Qua-qua-qua!");
+        }
+
+        public override void Swim()
+        {
+            Console.WriteLine("I can swim!");
+        }
+
+        public override void Fly()
+        {
+            throw new NotImplementedException("RubberDuck can not fly!");
+        }
+    }
+}
---
+++ hw3/WithoutLSP/WithoutLSP/WildDuck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WithoutLSP
+{
+    public class WildDuck: Duck
+    {
+        public override void Quack()
+        {
+            Console.WriteLine("Qua-qua-qua!");
+        }
+
+        public override void Swim()
+        {
+            Console.WriteLine("I can swim!");
+        }
+
+        public override void Fly()
+        {
+            Console.WriteLine("I can fly!");
+        }
+    }
+}
