c347dbfef5f455db0585a8fb2b6c1ed4cd4dc002
kogonidze <kottok1998@gmail.com>
2021-07-24 10:58:07 +0300

    + created asp.net core mvc project for EducationaCenter app

---
+++ EducationalCenter/EducationalCenter/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EducationalCenter.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
