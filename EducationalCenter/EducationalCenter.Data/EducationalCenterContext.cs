bf153d382b4a02e97b4ef5ed5fbce884f15fa17e
kogonidze <kottok1998@gmail.com>
2021-07-24 16:31:33 +0300

    * configured EF in .Data project

---
+++ EducationalCenter/EducationalCenter.Data/EducationalCenterContext.cs
@@ -0,0 +1,16 @@
+﻿using EducationalCenter.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducationalCenter.Data
+{
+    public sealed class EducationalCenterContext: DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public EducationalCenterContext( DbContextOptions<EducationalCenterContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ EducationalCenter/EducationalCenter.Model/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EducationalCenter.Model
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int EnrollmentYear { get; set; }
+    }
+}
