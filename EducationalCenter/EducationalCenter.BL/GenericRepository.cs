b0d8e08f99acad3c8a9d2e23b9e54d0cf50a6118
kogonidze <kottok1998@gmail.com>
2021-07-25 12:04:38 +0300

    + added generic repository

---
+++ EducationalCenter/EducationalCenter.BL/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EducationalCenter.IBL;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducationalCenter.BL
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity: class
+    {
+        private DbContext _context;
+        private DbSet<TEntity> _dbSet;
+
+        public GenericRepository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _dbSet.AsNoTracking().ToList();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public IEnumerable<TEntity> GetByFilter(Func<TEntity, bool> predicate)
+        {
+            return _dbSet.AsNoTracking().Where(predicate).ToList();
+        }
+
+        public void Create(TEntity item)
+        {
+            _dbSet.Add(item);
+            _context.SaveChanges();
+        }
+
+        public void Update(TEntity item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(TEntity item)
+        {
+            _dbSet.Remove(item);
+            _context.SaveChanges();
+        }
+    }
+}
