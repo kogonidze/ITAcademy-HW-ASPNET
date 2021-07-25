a591ac290cc9557511fb48ca4fcb7283d0dc3e93
kogonidze <kottok1998@gmail.com>
2021-07-25 14:22:57 +0300

    +added service layer to project, created StudentService

---
+++ EducationalCenter/EducationalCenter.ISL/IStudentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EducationalCenter.SL.DTO;
+
+namespace EducationalCenter.ISL
+{
+    public interface IStudentService
+    {
+        IEnumerable<StudentDTO> GetAll();
+    }
+}
---
+++ EducationalCenter/EducationalCenter.SL/StudentService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using EducationalCenter.IBL;
+using EducationalCenter.ISL;
+using EducationalCenter.Model;
+using EducationalCenter.SL.DTO;
+
+namespace EducationalCenter.SL
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IGenericRepository<Student> _repository;
+        private readonly IMapper _mapper;
+
+        public StudentService(IGenericRepository<Student> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public IEnumerable<StudentDTO> GetAll()
+        {
+            var students = _repository.GetAll();
+
+            return _mapper.Map<List<StudentDTO>>(students);
+        }
+    }
+}
