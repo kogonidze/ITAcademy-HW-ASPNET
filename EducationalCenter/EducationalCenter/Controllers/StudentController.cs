5f3dcde2900cb8705d512c687b273285ecff1a19
kogonidze <kottok1998@gmail.com>
2021-07-25 14:26:30 +0300

    + added controller for student, added view with list of students, removed Privacy view

---
+++ EducationalCenter/EducationalCenter/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EducationalCenter.BL;
+using EducationalCenter.IBL;
+using EducationalCenter.ISL;
+using EducationalCenter.Model;
+using EducationalCenter.SL;
+
+namespace EducationalCenter.Controllers
+{
+    public class StudentController : Controller
+    {
+        private IStudentService _service;
+
+        public StudentController(IStudentService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            var obj = _service.GetAll();
+            return View(obj);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
