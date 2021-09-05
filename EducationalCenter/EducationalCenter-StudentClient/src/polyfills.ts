f69847f6695d48955d096d942af99ff79cd69a93
kogonidze <kottok1998@gmail.com>
2021-09-05 12:29:51 +0300

    + created empty angular project EducationalCenter-StudentClient

---
+++ EducationalCenter/EducationalCenter-StudentClient/src/polyfills.ts
@@ -0,0 +1,14 @@
+/***************************************************************************************************
+ * BROWSER POLYFILLS
+ */
+
+// import 'classlist.js';  // Run `npm install --save classlist.js`.
+
+// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+
+import 'zone.js/dist/zone';  // Included with Angular CLI.
+
+
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
