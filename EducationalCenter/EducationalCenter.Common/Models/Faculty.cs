2227e2e9f94ce4fe75b24bb38f80bab082032257
kogonidze <kottok1998@gmail.com>
2021-09-12 13:57:26 +0300

    * added facultyId column to department

--- EducationalCenter/EducationalCenter.Common/Models/Faculty.cs
+++ EducationalCenter/EducationalCenter.Common/Models/Faculty.cs
@@ -1,9 +1,13 @@
-﻿namespace EducationalCenter.Common.Models
+﻿using System.Collections.Generic;
+
+namespace EducationalCenter.Common.Models
 {
     public class Faculty
     {
         public int Id { get; set; }
 
         public string Name { get; set; }
+
+        public IEnumerable<Department> Departments { get; set; }
     }
 }
