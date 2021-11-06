12d89c60d4a44280f70c348d76ba7d9eeb89b7c3
DESKTOP-7SB5UQQ\Vasili <kottok1998@gmail.com>
2021-11-06 22:51:57 +0300

    + implemented pagination for teachers on the client side

--- EducationalCenter/EducationalCenter.Angular/ClientApp/src/app/teachers/index-teachers/index-teachers.component.ts
+++ EducationalCenter/EducationalCenter.Angular/ClientApp/src/app/teachers/index-teachers/index-teachers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from 'src/app/shared/enums/category';
 import { Formation } from 'src/app/shared/enums/formation';
+import { PagedResult } from 'src/app/shared/models/pagedResult.model';
 import { Teacher } from 'src/app/shared/models/teacher/teacher.model';
 import { TeachersService } from '../teachers.service';
 
@@ -16,16 +17,27 @@ export class IndexTeachersComponent implements OnInit {
   teachers: Teacher[] | undefined;
   Category = Category;
   Formation = Formation;
+
+  countOfTeachers: number;
+  pageSize: number;
+  pageSizeOptions: number[] = [5, 10, 20, 50];
+
+  currentPage: number;
+  
   
   columnsToDisplay = ["fio", "dateOfBirth", "department", "experience", "category", "formation", "actions"];
 
   ngOnInit() {
+    this.pageSize = 10;
+    this.currentPage = 1;
+
     this.loadTeachers();
   }
 
   loadTeachers() {
-    this.teacherService.getAll().subscribe((teachers: Teacher[]) => {
-      this.teachers = teachers;
+    this.teacherService.getAll(this.currentPage + 1, this.pageSize).subscribe((response: PagedResult<Teacher[]>) => {
+      this.teachers = response.data;
+      this.countOfTeachers = response.countAllDocuments
     })
   }
 
@@ -33,5 +45,12 @@ export class IndexTeachersComponent implements OnInit {
     this.teacherService.delete(id).subscribe(() => {
         this.loadTeachers()
       });
+  }
+
+  handlePage(e: any) {
+    this.currentPage = e.pageIndex;
+    this.pageSize = e.pageSize;
+
+    this.loadTeachers();
   }
 }
